719d9772a43dfdc06a8042e06c9bf1c06f86e82c
Sreerag M S <sreeragmsadanandan@gmail.com>
2021-11-24 22:20:17 +0530

    Removed unused files

--- app/javascript/src/components/Main.jsx
+++ app/javascript/src/components/Main.jsx
@@ -1,70 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import { PageLoader } from "neetoui/v2";
-import PropTypes from "prop-types";
-import * as R from "ramda";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
-import { setAuthHeaders, registerIntercepts } from "apis/axios";
-import { initializeLogger } from "common/logger";
-import Login from "components/Authentication/Login";
-import PasswordReset from "components/Authentication/ResetPassword";
-import Signup from "components/Authentication/Signup";
-import PrivateRoute from "components/Common/PrivateRoute";
-import Dashboard from "components/Dashboard";
-import Hero from "components/Home/Hero";
-import { useAuthState, useAuthDispatch } from "contexts/auth";
-import { useUserDispatch, useUserState } from "contexts/user";
+import Home from "components/Home";
 
-const Main = props => {
-  const [loading, setLoading] = useState(true);
-  const { authToken } = useAuthState();
-  const { user: userContextState } = useUserState();
-  const userDispatch = useUserDispatch();
-  const authDispatch = useAuthDispatch();
-  const currentUser = userContextState || props?.user;
-  const isLoggedIn = !R.apply(
-    R.or,
-    R.map(R.either(R.isNil, R.isEmpty), [authToken, currentUser])
-  );
-
-  useEffect(() => {
-    userDispatch({ type: "SET_USER", payload: { user: props?.user } });
-    initializeLogger();
-    registerIntercepts(authDispatch);
-    setAuthHeaders(setLoading);
-  }, [authDispatch, props?.user, userDispatch]);
-
-  if (loading) {
-    return (
-      <div className="h-screen">
-        <PageLoader />
-      </div>
-    );
-  }
-
+const Main = () => {
   return (
     <BrowserRouter>
       <ToastContainer />
       <Switch>
-        <Route exact path="/my/password/new" component={PasswordReset} />
-        <Route exact path="/signup" component={Signup} />
-        <Route exact path="/login" component={Login} />
-        {!isLoggedIn && <Route exact path="/" component={Hero} />}
-        <PrivateRoute
-          path="/"
-          redirectRoute="/login"
-          condition={isLoggedIn}
-          component={Dashboard}
-        />
+        <Route exact path="/" component={Home} />
       </Switch>
     </BrowserRouter>
   );
-};
-
-Main.propTypes = {
-  user: PropTypes.object,
 };
 
 export default Main;
