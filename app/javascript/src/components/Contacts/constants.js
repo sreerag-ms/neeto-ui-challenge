1f01d22a413c592b6637503adfff71a1995c68c7
Sreerag M S <sreeragmsadanandan@gmail.com>
2021-11-26 23:15:25 +0530

    Contact key added for selection

--- app/javascript/src/components/Contacts/constants.js
+++ app/javascript/src/components/Contacts/constants.js
@@ -7,10 +7,13 @@ export const SIDE_MENU_ITEMS = {
   ],
 };
 
-export const TABLE_DATA = Array(21).fill({
-  name: "Some name",
-  role: "Some role",
-  imageUrl: "https://i.pravatar.cc/300",
-  createdAt: 1637933588000,
-  email: "mail@some.com",
-});
+export const TABLE_DATA = Array(21)
+  .fill(null)
+  .map((val, index) => ({
+    key: index,
+    name: `Some name ${index + 1}`,
+    role: "Some role",
+    imageUrl: index % 2 && "https://i.pravatar.cc/300",
+    createdAt: 1637933588000,
+    email: "mail@some.com",
+  }));
