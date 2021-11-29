bfed922c61e66a365ad7a94bf9dfb4f41499b384
Sreerag M S <sreeragmsadanandan@gmail.com>
2021-11-29 10:43:21 +0530

    Added logo to sidebar, File changes

--- app/javascript/src/components/Common/SideBar/index.jsx
+++ app/javascript/src/components/Common/SideBar/index.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 
-import { Text, NeetoInsights, UserCircle } from "@bigbinary/neeto-icons";
 import { Sidebar } from "@bigbinary/neetoui/v2/layouts";
+
+import { NAV_LINKS, PROFILE_INFO } from "./constants";
+import Logo from "./Logo";
 
 const SideBar = () => {
   const [isCollapsed, setIsCollapsed] = useState(true);
@@ -11,32 +13,9 @@ const SideBar = () => {
         setIsCollapsed(!isCollapsed);
       }}
       isCollapsed={isCollapsed}
-      profileInfo={{
-        imageUrl: "https://randomuser.me/api/portraits/women/90.jpg",
-        name: "Oliver Smith",
-        dropdownProps: [
-          {
-            label: "Edit",
-          },
-        ],
-      }}
-      navLinks={[
-        {
-          icon: Text,
-          label: "Notes",
-          to: "/notes",
-        },
-        {
-          icon: UserCircle,
-          label: "Contacts",
-          to: "/contacts",
-        },
-        {
-          icon: NeetoInsights,
-          label: "Somethng else",
-          to: "/something_else",
-        },
-      ]}
+      profileInfo={PROFILE_INFO}
+      navLinks={NAV_LINKS}
+      organizationInfo={{ logo: <Logo /> }}
     />
   );
 };
